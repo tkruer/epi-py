3ab83110eee8f7afafda0200b12fa6b023b6806f
Tyler K <tylerkruer1@gmail.com>
2022-11-29 18:39:20 -0700

    quick push

---
+++ src/reader/__main__.py
@@ -0,0 +1,12 @@
+# __main__.py
+
+import sys
+from reader import twobytwo
+
+def main():
+    twobytwo()
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/reader/twobytwo.py
@@ -0,0 +1,4 @@
+
+
+def twobytwo(a: int, b: int, c: int, d: int):
+    return a, b, c, d
